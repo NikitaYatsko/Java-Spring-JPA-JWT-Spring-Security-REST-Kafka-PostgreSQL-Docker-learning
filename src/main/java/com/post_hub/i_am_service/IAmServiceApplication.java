9544a838307639933c493a1b70e7e043675841df
Nick <archon04@mail.ru>
2025-10-14 13:51:18 +0300

    initialized project, added configurations,libraries and set up database

---
+++ src/main/java/com/post_hub/i_am_service/IAmServiceApplication.java
@@ -0,0 +1,13 @@
+package com.post_hub.i_am_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IAmServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IAmServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/post_hub/i_am_service/IAmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.post_hub.i_am_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IAmServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
