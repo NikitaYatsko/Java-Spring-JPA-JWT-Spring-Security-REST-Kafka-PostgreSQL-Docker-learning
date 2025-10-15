326367ab555b6120a2b8887c26954f236a5c4cca
Nick <archon04@mail.ru>
2025-10-15 13:44:32 +0300

    added MapStruct and automapping

--- src/main/java/com/post_hub/i_am_service/IAmServiceApplication.java
+++ src/main/java/com/post_hub/i_am_service/IAmServiceApplication.java
@@ -2,6 +2,8 @@ package com.post_hub.i_am_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
 public class IAmServiceApplication {
--- src/main/java/com/post_hub/i_am_service/service/Impl/PostServiceImpl.java
+++ src/main/java/com/post_hub/i_am_service/service/Impl/PostServiceImpl.java
@@ -1,5 +1,6 @@
 package com.post_hub.i_am_service.service.Impl;
 
+import com.post_hub.i_am_service.mapper.PostMapper;
 import com.post_hub.i_am_service.model.constants.ApiErrorMessage;
 import com.post_hub.i_am_service.model.dto.Post.PostDTO;
 import com.post_hub.i_am_service.model.entity.Post;
@@ -8,28 +9,23 @@ import com.post_hub.i_am_service.model.response.IamResponse;
 import com.post_hub.i_am_service.repositories.PostRepository;
 import com.post_hub.i_am_service.service.PostService;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
+
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class PostServiceImpl implements PostService {
-
-    private PostRepository postRepository;
+    private final PostRepository postRepository;
+    private final PostMapper postMapper;
 
     @Override
     public IamResponse<PostDTO> getById(@NotNull Integer postId) {
         Post post = postRepository.findById(postId).orElseThrow(
                 () -> new NotFoundException(ApiErrorMessage.POST_NOT_FOUND_BY_ID.getMessage(postId)));
 
-        PostDTO postDTO = PostDTO.builder()
-                .id(post.getId())
-                .title(post.getTitle())
-                .content(post.getContent())
-                .likes(post.getLikes())
-                .created(post.getCreated())
-                .build();
+        PostDTO postDTO = postMapper.toPostDto(post);
         return IamResponse.createSuccesful(postDTO);
     }
 }
